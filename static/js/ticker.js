b8275edbb4c42b20d08eb333147536f5c998dd09
Brad Fowler <bradley.fowler123@gmail.com>
2019-08-12 14:15:03 +0100

    Career tabs now only autochange if project is on or below screen. Tab items are options, can have 1, two or 3

--- static/js/ticker.js
+++ static/js/ticker.js
@@ -1,7 +1,9 @@
 const tabChange = function(container, new_tab_name, stop_auto_switch) {
+	// if user clicks on a tab
 	if (stop_auto_switch===undefined) stop_auto_switch = true;
+	// stop automatic switching between tabs forever
 	if (stop_auto_switch) window.clearTimeout(switcher);
-
+	// change tab
 	$(container).find(".tab-menu > .active, .tabs > .active").removeClass("active")
 	$(container).find(".tab-menu > [data-tab='"+new_tab_name+"'], .tabs > [data-tab='"+new_tab_name+"']").addClass("active")
 }
@@ -9,9 +11,16 @@ const tabChange = function(container, new_tab_name, stop_auto_switch) {
 
 function doTheSwitch() {
 	$(".tab-menu").each(function(i, t) {
-		const next_item = ($(t).find(".active").index() + 1) % $(t).children().length
-		const next_item_name = $(t).children()[next_item].dataset.tab
-		tabChange(t.parentElement, next_item_name, false)
+		t = $(t)
+		// if project is on or below the screen
+		if (t.parent().offset().top - $(window).scrollTop() > 0 ) {
+			// if the next tab
+			const next_item = (t.find(".active").index() + 1) % t.children().length
+			const next_item_name = t.children()[next_item].dataset.tab
+			// switch to it
+			tabChange(t.parent(), next_item_name, false)
+		}
 	})
 }
-const switcher = window.setInterval(doTheSwitch, 3000)
+const switcher = window.setInterval(doTheSwitch, 5000)
+$(document).ready(doTheSwitch)
